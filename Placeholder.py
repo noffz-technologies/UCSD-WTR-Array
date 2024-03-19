b4f711cda46b57de846f081ff8f58f5700202b86
catnip-noffz <150289958+catnip-noffz@users.noreply.github.com>
2024-03-19 14:09:02 -0500

    Initial test upload

---
+++ Placeholder.py
@@ -0,0 +1,41 @@
+import nidaqmx
+import pyvisa
+
+# General function (logging_enable, error_enable, test_number, date, etc.)
+
+# Power sequence (notify user to validate power supply procedure)
+#   Power supply
+    # Enable power
+    # Validate power on rails is appropriate
+
+# -------------------------------------------------------------------------------
+# Instrument control parameters read from config.txt ()
+#   Synths
+    # Center frequency
+    # Power
+
+#   Downconverters
+    # Values from table to set filters
+
+#   VSGs
+    # Cal signal file
+    # Center frequency
+    # Power
+    # Symbol rate
+
+#   Scopes
+    # Center Frequency
+    # Bandwidth
+    # Sample rate
+    # Collection length
+    # Collection time
+
+    # *Sync precision time
+
+# Data Collection
+    # Start
+    # Store data + metadata from instruments
+
+# -------------------------------------------------------------------------------
+# Cleanup
+    # Disable power
